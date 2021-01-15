b7569f50842784412102ff138aa0cb6f952c8b62
Gerzogb <63235955+Gerzogb@users.noreply.github.com>
2021-01-15 17:05:14 +0500

    есть уровень и игрок не уходит за экран

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
 import sys
 import pygame
+import pytmx
 
 
 pygame.init()
-size = width, height = 480, 270
+size = width, height = 960, 800
 screen = pygame.display.set_mode(size)
 
 
@@ -23,7 +24,7 @@ def load_image(name, colorkey=None):
         image.set_colorkey(colorkey)
     else:
         image = image.convert_alpha()
-    image = pygame.transform.scale(image, (200, 200))
+    image = pygame.transform.scale(image, (32, 32))
     return image
 
 
@@ -33,16 +34,39 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, *groups):
         super().__init__(*groups)
         self.rect = self.image.get_rect()
+        self.rect.x = 32
+        self.rect.y = 16 * 32
         self.speedx = 0
 
     def go_horizont(self):
         self.speedx = 0
+        if self.rect.x < 0: #чтобы за экран не уходил
+            self.rect.x = 0
+        elif self.rect.x > 928:
+            self.rect.x = 928
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT]:
             self.speedx = -3 # float не работает
         if key[pygame.K_RIGHT]:
             self.speedx = 3
         self.rect.x += self.speedx
+
+
+class Level:
+    def __init__(self):
+        self.map = pytmx.load_pygame("data/map.tmx")
+        self.height = self.map.height
+        self.width = self.map.width
+        self.tile_size = 32
+
+    def render(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                image = self.map.get_tile_image(x, y, 0)
+                screen.blit(image, (x * self.tile_size, y * self.tile_size))
+
+    def get_tileid(self):
+        pass
 
 
 running = True
@@ -53,8 +77,11 @@ all_sprites = pygame.sprite.Group()
 player = Player(all_sprites)
 all_sprites.add(player)
 
+lvl = Level()
+
 while running:
     clock.tick(FPS)
+    lvl.render()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
