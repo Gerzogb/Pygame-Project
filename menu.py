c44e9d93bfc1ef9838f784cebaf063d1327475fe
Арсений <suvorovars@gmail.com>
2021-01-17 18:36:19 +0500

    сделал вариант меню на QT

---
+++ menu.py
@@ -0,0 +1,20 @@
+import sys
+
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+class MyWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('menu.ui', self)  # Загружаем дизайн
+        self.btn_play.clicked.connect(self.play)
+
+    def play(self):
+        import main
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
